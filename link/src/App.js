5088ad97914f0d8c7dbf932359cdcc09f13bb708
douglasweb <webdouglasti@gmail.com>
2020-12-28 23:04:39 -0300

    Criacao do projeto

---
+++ link/src/App.js
@@ -0,0 +1,19 @@
+import { Grid } from "@material-ui/core";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Grid
+      className="App"
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+    >
+      <h1>Gerador de link</h1>
+    </Grid>
+  );
+}
+
+export default App;
